3571822fc489bc4bf419ffecf41e4f7228264ec8
christinaiev <58794390+christinaiev@users.noreply.github.com>
2026-04-22 17:54:55 +0100

    Initial commit, signals Go project backend with functional API for fetching tracks, signals, track specific data and all the tracks sharing a signal.

---
+++ Backend/Signals/server.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+type Signal struct {
+	Id      int64   `json:"signal_id"`
+	Name    string  `json:"signal_name"`
+	Elr     string  `json:"elr"`
+	Mileage float32 `json:"mileage"`
+}
+
+type UniqueSignal struct {
+	Id      int64     `json:"signal_id"`
+	Name    string    `json:"signal_name"`
+	Elr     string    `json:"elr"`
+	Mileage []float32 `json:"mileage"`
+}
+
+type Track struct {
+	Id      int64    `json:"track_id"`
+	Source  string   `json:"source"`
+	Target  string   `json:"target"`
+	Signals []Signal `json:"signal_ids"`
+}
+
+var tracks []Track
+
+//var tracks = map[int]*Track{}
+
+func getAllTracks(c *echo.Context) error {
+	return c.JSON(http.StatusOK, tracks)
+}
+
+func getTrack(c *echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var foundIndex int = -1
+	for index := range tracks {
+		if tracks[index].Id == int64(id) {
+
+			foundIndex = index
+			fmt.Println("found track at ", index)
+			break
+		}
+	}
+
+	if foundIndex == -1 {
+		return c.JSON(http.StatusOK, "")
+	}
+
+	sort.Slice(tracks[foundIndex].Signals[:], func(i, j int) bool {
+		return tracks[foundIndex].Signals[i].Id < tracks[foundIndex].Signals[j].Id
+	})
+	return c.JSON(http.StatusOK, tracks[foundIndex])
+
+}
+
+func getSignal(c *echo.Context) error {
+	track_id, _ := strconv.Atoi(c.Param("track_id"))
+	signal_id, _ := strconv.Atoi(c.Param("signal_id"))
+
+	var foundTrack int = -1
+	for index := range tracks {
+		if tracks[index].Id == int64(track_id) {
+			foundTrack = index
+			break
+		}
+	}
+
+	var foundSignal int = -1
+	for index := range tracks[foundTrack].Signals {
+		if tracks[foundTrack].Signals[index].Id == int64(signal_id) {
+			foundSignal = index
+			break
+		}
+	}
+
+	return c.JSON(http.StatusOK, tracks[foundTrack].Signals[foundSignal])
+}
+
+func getAllSignals(c *echo.Context) error {
+	var Signals = map[int64]*UniqueSignal{}
+
+	for indexTracks := range tracks {
+		for indexSignal := range tracks[indexTracks].Signals {
+
+			_, exists := Signals[tracks[indexTracks].Signals[indexSignal].Id]
+			if !exists {
+				Signals[tracks[indexTracks].Signals[indexSignal].Id] = &UniqueSignal{}
+				CurrentSignal := Signals[tracks[indexTracks].Signals[indexSignal].Id]
+				CurrentSignal.Id = tracks[indexTracks].Signals[indexSignal].Id
+				CurrentSignal.Name = tracks[indexTracks].Signals[indexSignal].Name
+				CurrentSignal.Elr = tracks[indexTracks].Signals[indexSignal].Elr
+
+				CurrentSignal.Mileage = append(CurrentSignal.Mileage, tracks[indexTracks].Signals[indexSignal].Mileage)
+			}
+			if exists {
+				CurrentSignal := Signals[tracks[indexTracks].Signals[indexSignal].Id]
+				CurrentSignal.Mileage = append(CurrentSignal.Mileage, tracks[indexTracks].Signals[indexSignal].Mileage)
+			}
+
+		}
+	}
+	result := make([]UniqueSignal, 0, len(Signals))
+	for _, signal := range Signals {
+		result = append(result, *signal)
+	}
+	sort.Slice(result[:], func(i, j int) bool {
+		return result[i].Id < result[j].Id
+	})
+
+	return c.JSON(http.StatusOK, result)
+}
+
+func getSignalTracks(c *echo.Context) error {
+	signal_id, _ := strconv.Atoi(c.Param("id"))
+
+	fmt.Println("Tracing getSignalTracks for ", signal_id)
+	var signalTracks []Track
+
+	for indexTracks := range tracks {
+		for indexSignal := range tracks[indexTracks].Signals {
+			fmt.Println("Iterate signal:", tracks[indexTracks].Id, " ", tracks[indexTracks].Signals[indexSignal].Id)
+			if tracks[indexTracks].Signals[indexSignal].Id == int64(signal_id) {
+				signalTracks = append(signalTracks, tracks[indexTracks])
+				fmt.Println("Found track:", tracks[indexTracks].Id)
+				break
+			}
+		}
+	}
+	sort.Slice(signalTracks[:], func(i, j int) bool {
+		return signalTracks[i].Id < signalTracks[j].Id
+	})
+	return c.JSON(http.StatusOK, signalTracks)
+}
+
+func loadJSONFile(fileName string) {
+
+	jsonFile, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	var cleanedJson string = strings.Replace(string(byteValue), "NaN", "null", -1)
+
+	err = json.Unmarshal([]byte(cleanedJson), &tracks)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return
+	}
+	sort.Slice(tracks[:], func(i, j int) bool {
+		return tracks[i].Id < tracks[j].Id
+	})
+	fmt.Println("Tracks loaded ", tracks)
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.RequestLogger())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:5173"},
+	}))
+
+	loadJSONFile("crosstech-test-data.json")
+
+	e.GET("/tracks/", getAllTracks)
+	e.GET("/track/:id", getTrack)
+	e.GET("/track/:track_id/signal/:signal_id", getSignal)
+	e.GET("/signals/", getAllSignals)
+	e.GET("/signaltracks/:id", getSignalTracks)
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to the Signals App!")
+	})
+
+	if err := e.Start(":8080"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+
+}
